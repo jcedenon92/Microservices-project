536641c05961fa31f603709ecf56ecaf721278d0
Jorge Cedeño Ñique <jcedenon92@gmail.com>
2022-06-20 23:28:07 -0500

    feature upgrade Patient Microservice

---
+++ ms-patient-controls-management/src/main/java/com/nutrition/app/java/ms/sc/mspatientcontrolsmanagement/MsPatientControlsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.nutrition.app.java.ms.sc.mspatientcontrolsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPatientControlsManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPatientControlsManagementApplication.class, args);
+    }
+
+}
